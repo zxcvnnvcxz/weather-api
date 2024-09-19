8fcd092b85e6da5692abd1f9817ccf6081bc6939
EDWARD <edward.kang0301@gmail.com>
2024-09-20 03:53:22 +1000

    Added __main__ to only execute when main is run. Added temp static dictionary

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template
-
-app = Flask("Website")
+import pandas as pd
+app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    # df = pd.read_csv("")
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
